f081d4bacba3656e27d98915220698ece47525d8
Hxsolo2 <2002190C@student.tp.edu.sg>
2021-11-28 17:04:15 +0800

    update

--- index.php
+++ index.php
@@ -3,8 +3,19 @@
     <title>PHPLab1</title>
 </head>
 <body>
+
     <?php
       echo "HELLO WORLD!!!";
     ?>
+
+    <div class="header">
+       <a href="#logo" class="Logo">FRecords.Co</a>
+       <div class="sections">
+         <a href="#login">Login</a>
+         <a href="#feedback">Feedback</a>
+         <a href="#about">About</a>
+       </div>
+    </div>
+
 </body>
 </html>

4c5fd31289844a674351fcfa1f891460b0bfed70
Marcus <2000323b@student.tp.edu.sg>
2021-11-28 20:41:52 +0800

    Nav Bar Changes

--- index.php
+++ index.php
@@ -1,19 +1,28 @@
 <html>
+
 <head>
-    <title>PHPLab1</title>
+    <title>PHP Website</title>
     <link rel="stylesheet" href="Header.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 </head>
+
 <body>
+    <div class="container-fluid">
+        <nav class="navbar navbar-light bg-light ">
+            <a class="navbar-brand" href="index.php">
+                FRecords.co
+            </a>
+        </nav>
+        <nav class="navbar navbar-light bg-light justify-content-end">
+            <button type="button" class="btn btn-outline-dark rfloat" href="login.php">Login</button>
+            <button type="button" class="btn btn-outline-dark rfloat" href="feedback.php">Feedback</button>
+            <button type="button" class="btn btn-outline-dark rfloat" href="about.php">About</button>
+            <button type="button" class="btn btn-outline-dark rfloat" href="index.php">Home</button>
+        </nav>
 
-    <div class="header">
-       <a href="#logo" class="Logo">FRecords.Co</a>
-       <div class="sections">
-         <a href="#login">Login</a>
-         <a href="#feedback">Feedback</a>
-         <a href="#about">About</a>
-         <a href="#home" class="active">Home</a>
-       </div>
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
     </div>
-
 </body>
+
 </html>

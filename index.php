6fae12fc6790f4e27c08a2ce4a93a1b495af82d0
Marcus <2000323b@student.tp.edu.sg>
2021-11-28 15:35:25 +0800

    Table test

--- index.php
+++ index.php
@@ -11,42 +11,38 @@ table, th, td {
 <body>
 <table style="width:100%">
   <tr>
-    <th>Course ID</th>
-    <th>Course Name</th>
-    <th>Enrollment</th>
+    <th></th>
+    <th></th>
+    <th></th>
   </tr>
   <tr>
-    <td>15459</td>
-    <td>Introduction to Linear Algebra</td>
-    <td>21</td>
-  </tr>
-  <tr>
-    <td>15558</td>
-    <td>Calculus I</td>
-    <td>42</td>
-  </tr>
-  <tr>
-    <td>15559</td>
-    <td>Calculus II</td>
-    <td>29</td>
-  </tr>
-  <tr>
-    <td>15560</td>
-    <td>Calculus III</td>
-    <td>20</td>
-  </tr>
-  <tr>
-    <th>Total</th>
     <td></td>
-    <td>92</td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <th></th>
+    <td></td>
+    <td></td>
   </tr>
 </table>
 <?php
-<<<<<<< HEAD
 echo "Hello BREAlD! HI";
-=======
-echo "Hello BREAD! Hi";
->>>>>>> a18bd44bcf220d756839723af9f6254706d83bc9
 ?>
 </body>
 </html>

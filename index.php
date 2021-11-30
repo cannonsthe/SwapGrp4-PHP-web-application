d518e4652a940880a09ce5b43f399755d1c65dcd
Hxsolo2 <2002190C@student.tp.edu.sg>
2021-11-30 13:20:50 +0800

    Main Page

--- index.php
+++ index.php
@@ -6,16 +6,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FRecords</title>
     <link rel="stylesheet" href="HomePage.css">
+    <script src="https://kit.fontawesome.com/c084a746b6.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <nav>
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn">
+           <i class="menu"></i>
+        </label>
        <label class="logo">FRecords</label>
        <ul>
-           <li><a href="#">Home</a></li>
+           <li><a class="selected" href="#">Home</a></li>
            <li><a href="#">Feedback</a></li>
            <li><a href="#">Login</a></li>
            <li><a href="#">About</a></li>
        </ul>
    </nav>
+   <section></section>
 </body>
 </html>

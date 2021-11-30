a47ac3cb5068a91a27ee0186ba5eb8f1e433fd2c
Marcus <2000323b@student.tp.edu.sg>
2021-11-30 23:31:35 +0800

    Login Page Html and css draft

--- login.php
+++ login.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="HomePage.css">
+    <link rel="stylesheet" href="LoginPage.css">
     <script src="https://kit.fontawesome.com/c084a746b6.js" crossorigin="anonymous"></script>
 </head>
 <body>
@@ -18,6 +19,31 @@
            <a href="feedback.php">Feedback</a>
        </div>
     </div>
+    <div class="wrapper fadeInDown">
+  <div id="formContent">
+    <!-- Tabs Titles -->
+    <h2 class="active"> Sign In </h2>
+    <h2 class="inactive underlineHover">Sign Up </h2>
+
+    <!-- Icon -->
+    <div class="fadeIn first">
+      <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" />
+    </div>
+
+    <!-- Login Form -->
+    <form>
+      <input type="text" id="login" class="fadeIn second" name="login" placeholder="login">
+      <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
+      <input type="submit" class="fadeIn fourth" value="Log In">
+    </form>
+
+    <!-- Remind Passowrd -->
+    <div id="formFooter">
+      <a class="underlineHover" href="#">Forgot Password?</a>
+    </div>
+
+  </div>
+</div>
 
 </body>
 </html>

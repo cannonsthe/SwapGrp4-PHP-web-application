90cbe24c8dd1d570dcd2c92092aef4fd6e65cb72
Lxtfia <nurlutfiadini03@gmail.com>
2021-12-01 00:54:18 +0800

    about added

--- about.php
+++ about.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About</title>
     <link rel="stylesheet" href="HomePage.css">
+    <link rel="stylesheet" href="AboutPage.css">
 </head>
 <body>
     <div class="navbar">
@@ -17,6 +18,20 @@
            <a href="feedback.php">Feedback</a>
        </div>
     </div>
-
+    <div class="container">
+        <h1 style="text-align:center;line-height:2px;font-size:70px;">About FRecords</h1>
+        <p style="text-align:justify;padding-top:2px;font-size:18px;">Ay How you feelin' out there ATEEZ Present 거 이놈아 Go 이놈아 거 이놈아 Go 이놈아 거 이놈아 Go 이놈아 이게 바로 멋인 기라 아따 고마해라 느그들의 멋 아이고 하나 부질없다
+        건들건들 거 틀려먹은 type 부단히 빈 수레를 굴려 요란하다 흠 그래그래 꼴사납지 참 눈썹을 좀 더 찌푸려 But 인사는 90도 바지를 좀 더 내려
+        발걸음은 Not Dum di li dum Dum di li dum dum 우린 뜨겁게 살아 허나 누구보다 더 Humble & Kindness 내 신념을 지켜 누구든지 고이 모셔
+        자센 낮게 눈빛은 Keep it Up up up up 이런 게 바로 멋인 기라 Welcome my class 다 새겨들어 다 우릴 봐 우리 춤을 봐 Wow
+        더리덤 더리덤 더리덤덤 더리덤 더리더리 더덤 더리덤 더리덤 더리덤덤 우릴 봐 우리 춤을 봐 Wow 이 드라마에 출처 주인공인 줄 알겠지
+        죽어도 몰라 끝이 보일 때 깨닫지 인정 못 한 진실 현실은 다 알지 곧 끝났단 걸 무너질 때 네 분수를 알지 너 그럼 안 돼 (I talk about you)
+        그래선 안 돼 (그래선 안 돼) 진짜는 말이야 (Real recognize real) 겸손 겸손 우린 뜨겁게 살아 허나 누구보다 더 Humble & Kindness
+        내 신념을 지켜 누구든지 고이 모셔 자센 낮게 눈빛은 Keep it Up up up up 이것이 멋이여 Welcome my class 다 새겨들어 다
+        우릴 봐 우리 춤을 봐 Wow 더리덤 더리덤 더리덤덤 더리덤 더리더리 더덤 더리덤 더리덤 더리덤덤 우릴 봐 우리 춤을 봐 Wow
+        숙여라 익을수록 입은 놀리지 마라 익을수록 머릴 숙여라 익을수록 너네 잘 배워놔라 익을수록 숙여라 익을수록 불지 않아 라면
+        시간이 지날수록 늘어나는 과녁 태도는 정중해 자신과 신념을 위해 매 순간을 치고받아 춤 들어가유 우릴 봐 우리 춤을 봐 Wow
+        Welcome my class 다 새겨들어 다 우릴 봐 우리 춤을 봐 Wow 더리덤 더리덤 더리덤덤 더리덤 더리더리 더덤 더리덤 더리덤 더리덤덤</p>  
+    <div>
 </body>
 </html>

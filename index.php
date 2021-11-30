c3367f8c0c2e537a3506b30d96409e92ce427d6c
Hxsolo2 <2002190C@student.tp.edu.sg>
2021-11-30 12:05:20 +0800

    changes

--- index.php
+++ index.php
@@ -1,26 +1,21 @@
-<html>
-
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FRecords</title>
-    <link rel="stylesheet" href="Homeheader.css">
+    <link rel="stylesheet" href="HomePage.css">
 </head>
-
 <body>
-
-    <div class="navbar ">
-        <a class="navbar-logo ">
-            FRecords.co
-
-        </a>
-        <a class="navbar-btn">
-            <button type="button" class="Login-btn" href="login.php">Login</button>
-            <button type="button" class="Feedback-btn" href="feedback.php">Feedback</button>
-            <button type="button" class="About-btn" href="about.php">About</button>
-            <button type="button" class="Home-btn" href="index.php">Home</button>
-        </a>
-    </div>
-
-    
+    <nav>
+       <label class="logo">FRecords</label>
+       <ul>
+           <li><a href="#">Home</a></li>
+           <li><a href="#">Feedback</a></li>
+           <li><a href="#">Login</a></li>
+           <li><a href="#">About</a></li>
+       </ul>
+   </nav>
 </body>
-
 </html>

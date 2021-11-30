e0ff589bd52ee465059260b8f051bbd81a83977b
Marcus <2000323b@student.tp.edu.sg>
2021-11-30 23:34:32 +0800

    Modified Draft

--- login.php
+++ login.php
@@ -23,7 +23,6 @@
   <div id="formContent">
     <!-- Tabs Titles -->
     <h2 class="active"> Sign In </h2>
-    <h2 class="inactive underlineHover">Sign Up </h2>
 
     <!-- Icon -->
     <div class="fadeIn first">
@@ -36,11 +35,6 @@
       <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
       <input type="submit" class="fadeIn fourth" value="Log In">
     </form>
-
-    <!-- Remind Passowrd -->
-    <div id="formFooter">
-      <a class="underlineHover" href="#">Forgot Password?</a>
-    </div>
 
   </div>
 </div>

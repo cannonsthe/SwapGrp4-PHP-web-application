14d6758c885d69b9534b21e4aedeef316ebfa05d
gabrielle <gabrielletan13@gmail.com>
2021-12-02 01:16:34 +0800

    Feedback Update

---
+++ debug.php
@@ -0,0 +1,51 @@
+<html>
+<style>
+table, th, td {
+  border:1px solid black;
+}
+
+</style>
+<head>
+<title>PHPLab1</title>
+</head>
+<body>
+<table style="width:100%">
+  <tr>
+    <th></th>
+    <th></th>
+    <th></th>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <th></th>
+    <td></td>
+    <td></td>
+  </tr>
+</table>
+<?php
+echo "HELLO WORLD!!!";
+?>
+<?php
+echo "HELLO WORLD!!!";
+?>
+</body>
+</html>

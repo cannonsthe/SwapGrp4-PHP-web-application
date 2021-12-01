1b8ee7e190a01f16da6fb356b6069712bc2fef69
Hxsolo2 <2002190C@student.tp.edu.sg>
2021-12-01 21:18:13 +0800

    Login Pages

---
+++ Salary.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Salary</title>
+    <link rel="stylesheet" href="Salary.css">
+</head>
+<body>
+
+    <div class="navbar">
+       <a href="#" class="logo">FRecords</a>
+       <div class="sections">
+           <a href="#" class="active">Salary Records</a>
+           <a href="Employee.php" >Employee Information</a>
+           <a href="Expenses.php" >Expenses</a>
+           <a href="CPF.php">CPF Records</a>
+           <a href="Logouy.php">Logout</a>
+       </div>
+    </div>
+    
+</body>
+</html>

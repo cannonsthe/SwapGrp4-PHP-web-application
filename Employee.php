2fd492db3f1ae89a7a37ee58e52b61336418fdbf
iffah <2004908F@student.tp.edu.sg>
2021-12-02 02:22:08 +0800

    Final Employee Information Changes

--- Employee.php
+++ Employee.php
@@ -20,16 +20,32 @@
        </div>
     </div>
 
-    <h1>Employee Information</h1>
+    <strong><p style="font-size:40px;margin-left:36%;">Employee Information</p><strong>
 
     <div class="container">
         <img src="/pictures/EmployeeInformation_UserInformation.png" id="icon" alt="User Icon" style="width:250px; margin-left:40%" />
         <form>
-            <label for="name" style="margin: 0px auto 0px auto;font-size: 20px;margin-bottom:0px;">Full Name</label>
-            <input type="text" disabled class="form-control" id="fullname">
+            <strong><label for="name" style="margin: 0px auto 0px auto;font-size: 20px;margin-bottom:0px;">Full Name</label></strong>
+            <input type="text" disabled class="form-control" id="Full Name">
             </br>
-            <label for="name" style="margin: 5px auto 0px auto;font-size: 20px;">Last Name</label>
-            <input type="text" disabled class="form-control" id="fullname" >
+            <strong><label for="password" style="margin: 5px auto 0px auto;font-size: 20px;">Password</label></strong>
+            <input type="text" disabled class="form-control" id="Password" >
+            </br>
+            <strong><label for="email" style="margin: 5px auto 0px auto;font-size: 20px;">Email</label></strong>
+            <input type="text" disabled class="form-control" id="Email" >
+            </br>
+            <strong><label for="department" style="margin: 5px auto 0px auto;font-size: 20px;">Department</label></strong>
+            <input type="text" disabled class="form-control" id="Departmen" >
+            </br>
+            <strong><label for="position" style="margin: 5px auto 0px auto;font-size: 20px;">Position</label></strong>
+            <input type="text" disabled class="form-control" id="Position" >
+            </br>
+            <strong><label for="accountbank" style="margin: 5px auto 0px auto;font-size: 20px;">Account Bank</label></strong>
+            <input type="text" disabled class="form-control" id="Account Bank" >
+            </br>
+            <strong><label for="accountnumber" style="margin: 5px auto 0px auto;font-size: 20px;">Account Number</label></strong>
+            <input type="text" disabled class="form-control" id="Account Number" >
+            
         </form>
     </div>
     

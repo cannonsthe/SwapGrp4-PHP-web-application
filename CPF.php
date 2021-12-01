1a877d7abaf7bc8fc6f6e4aa61be5d583a23d185
Lxtfia <nurlutfiadini03@gmail.com>
2021-12-02 00:29:44 +0800

    new

---
+++ CPF.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CPF</title>
+    <link rel="stylesheet" href="CPF.css">
+</head>
+<body>
+
+    <div class="navbar">
+       <a href="#" class="logo">FRecords</a>
+       <div class="sections">
+           <a href="Employee.php" >Employee Information</a>
+           <a href="Salary.php">Employee Salary</a>
+           <a href="Expenses.php" > General Expenses</a>
+           <a href="#" class="active">CPF Contributions</a>
+           <a href="index.php">Logout</a>
+       </div>
+    </div>
+
+    <h1>CPF Contributions</h1>
+
+</body>
+</html>

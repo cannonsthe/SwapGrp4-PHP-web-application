be8d96a87a1ec83e248966152a5dfb8ec8d758e8
Marcus <2000323b@student.tp.edu.sg>
2021-12-01 23:04:10 +0800

    GE

---
+++ Expenses.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FRecords</title>
+    <link rel="stylesheet" href="HomePage.css">
+</head>
+<body>
+    <div class="navbar">
+       <a href="#" class="logo">FRecords</a>
+       <div class="sections">
+           <a href="#" class="active">Home</a>
+           <a href="about.php" >About</a>
+           <a href="login.php" >Login</a>
+           <a href="feedback.php">Feedback</a>
+       </div>
+    </div>
+
+</body>
+</html>

6ea84ae55ccd0921035f5df2c103a57c3d4d89e9
Marcus <2000323b@student.tp.edu.sg>
2021-12-06 03:16:09 +0800

    Update working Expenses

--- Expenses.php
+++ Expenses.php
@@ -11,6 +11,12 @@
 </head>
 
 <body>
+    <?php
+    $con = mysqli_connect("localhost", "root", "", "swaprj"); //connect to database
+    if (!$con) {
+        die('Could not connect: ' . mysqli_connect_errno()); //return error is
+    }
+    ?>
     <div class="navbar">
         <a href="#" class="logo">FRecords</a>
         <div class="sections">
@@ -23,32 +29,48 @@
         </div>
     </div>
     <br><br>
-    <form action="/expenses_page.php" class="center">
-        <label for="month" class="center">Choose a year to display expenses for:</label>
-        <select name="month" id="month">
+    <form action="/Expenses.php" class="center">
+        <label for="year1" class="center">Choose a year to display expenses for:</label>
+        <select name="year" id="year">
+            <option value="All">All</option>
             <option value="2019">2019</option>
             <option value="2020">2020</option>
             <option value="2021">2021</option>
         </select>
         <br><br>
         <input type="submit" value="Retrieve">
+        
+        <?php
+        /* $year = $_REQUEST['year']; #########Test Retreive
+        echo $year;
+        $query = $con->prepare("select * from expenses WHERE year=?");
+        $query->execute();
+        $query->bind_result($expenseid, $year, $type, $amount);
+        echo "<table class='salary'><tr>";
+        echo
+        "<th>| Expense ID</th><th>| Year |</th><th>Type of Expense |</th><th> Amount |</t
+        h></tr>";
+        while ($query->fetch()) {
+            //do something
+            echo
+            "<th>$expenseid</th><th>$year</th><th>$type</th><th>$amount</t
+        h></tr>";
+        }
+        echo "</table>"; */
+        ?>
     </form>
     <?php
-    $con = mysqli_connect("localhost", "root", "", "swaprj"); //connect to database
-    if (!$con) {
-        die('Could not connect: ' . mysqli_connect_errno()); //return error is
-    }
     $query = $con->prepare("select * from expenses");
     $query->execute();
-    $query->bind_result($expenseid, $date, $type, $amount);
+    $query->bind_result($expenseid, $year, $type, $amount);
     echo "<table class='salary'><tr>";
     echo
-    "<th>| Expense ID</th><th>| Date |</th><th>Type of Expense |</th><th> Amount |</t
+    "<th>| Expense ID</th><th>| Year |</th><th>Type of Expense |</th><th> Amount |</t
     h></tr>";
     while ($query->fetch()) {
         //do something
         echo
-        "<th>$expenseid</th><th>$date</th><th>$type</th><th>$amount</t
+        "<th>$expenseid</th><th>$year</th><th>$type</th><th>$amount</t
     h></tr>";
     }
     echo "</table>";
@@ -56,17 +78,21 @@
 
 
     <br><br><br><br>
-    <form action="/Expenses_post.php" method="POST">
+    <form>
         <fieldset>
             <legend>Form:</legend>
-            
+
             <label for="year">Year of Expense:</label>
             <input type="text" id="year" name="year"><br><br>
             <label for="type">Type of Expense:</label>
             <input type="text" id="type" name="type"><br><br>
             <label for="amount">Amount:</label>
             <input type="text" id="amount" name="amount"><br><br>
-            <input type="submit" value="Create new">
+            <label for="expenseid">Expense ID (Only Use For Updating or Deleting!):</label>
+            <input type="text" id="expenseid" name="expenseid"><br><br>
+            <input type="submit" value="Create new" formmethod="POST" formaction="/Expenses_post.php">
+            <input type="submit" value="Update" formmethod="PUT" formaction="/Expenses_update.php">
+
         </fieldset>
     </form>
 
--- Expenses_post.php
+++ Expenses_post.php
@@ -4,18 +4,25 @@ if (!$con) {
     die('Could not connect: ' . mysqli_connect_errno()); //return error is connect fail
 }
 
-$date = $_REQUEST['year'];
+$year = $_REQUEST['year'];
 $type = $_REQUEST['type'];
 $amount = $_REQUEST['amount'];
+$expenseid = $_REQUEST['expenseid'];
+$method = $_SERVER['REQUEST_METHOD'];
 
-$query = $con->prepare("INSERT INTO `expenses` (`date`,`type`,`amount`) VALUES
-(?,?,?)");
 
-$query->bind_param('sss',$date, $type, $amount); //bind the parameters
-if ($query->execute()) { //execute query
-    header("Location: /Expenses.php");
-    echo '<script>alert("Entry Inserted!")</script>';
-} else {
-    echo "Error executing query.";
+if ($_POST) {
+    $query = $con->prepare("INSERT INTO `expenses` (`year`,`type`,`amount`) VALUES
+    (?,?,?)");
+
+    $query->bind_param('sss', $year, $type, $amount); //bind the parameters
+    if ($query->execute()) { //execute query
+        echo ("<script LANGUAGE='JavaScript'>
+        window.alert('Entry Inserted!');
+        window.location.href='/Expenses.php';
+        </script>");
+    } else {
+        echo "Error executing query.";
+    }
 }
-?>
+    $query->close();

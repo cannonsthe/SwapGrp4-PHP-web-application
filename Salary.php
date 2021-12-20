f9efa1b654197584d7910832b2c01778548324bd
Hxsolo2 <2002190C@student.tp.edu.sg>
2021-12-20 13:39:38 +0800

    Salary connected to database

--- Salary.php
+++ Salary.php
@@ -24,52 +24,54 @@
     <h1>Salary Records</h1>
     <p class="warning">These records are strictly confidential. Do not distribute or share without permission.Violators will be punished!</p>
 
-       <table class="salary">      <!--Table-->
-           <tr>
-              <td>Name</td> 
-              <td>Salary</td>
-              <td>Position</td>
-              <td>Id No.</td>
-            </tr>
-
-           <tr>
-              <td>Alex Tay</td> 
-              <td>$76,000 SGD</td>
-              <td>Finance Manager</td>
-              <td>29891746</td>
-           </tr>
-
-           <tr>
-              <td>Mary Kiat</td> 
-              <td>$78,000 SGD</td>
-              <td>Pricing Analyst</td>
-              <td>44568973</td>
-            </tr>
-
-           <tr>
-              <td>Eng Lai Fa</td> 
-              <td>$220,000 SGD</td>
-              <td>Finance Director</td>
-              <td>78635468</td>
-           </tr>
-        </table>
-
      <?php
-     //Creating mysql variables to specify attributes for mysql connection
-     $mysql_host="localhost";  //MySQL server's ip address
-     $mysql_user="root";   //MySQL server's user privilege
-     $mysql_password="";  //MySQL server's password
-     $mysql_db="swaprj"; //MySQL server's database name
      
-     
-     $con = mysqli_connect($mysql_host,$mysql_user,$mysql_password,$mysql_db);  //Open a connection to database server
+     $con = mysqli_connect("localhost","root","","swaprj");  //Open a connection to database server(localhost,user,password,dbname)
      
      //Check if the connection works or not
      if (!$con){
         die('Failed to connect: ' .mysqli_connect_errno()); //Terminate current script and show error code
      }
      else {
-        echo "Connection to server at $mysql_host successful<br>";  // Echo successful connection
+        echo "Connection to database server successful<br>";  // Echo successful connection
+     }
+
+     //Query swaprj database and select data from salary table
+     $query="SELECT userid, amount, position, fname FROM swaprj.salary"; //SELECT all the data from the columns in the salary table
+     $result=mysqli_query($con,$query); //mysqli_query executes the checking of the connection from $con and also gets the data from our salary table through $query, and then send that data to the $results variable
+     
+     //Getting the rows of data from the salary table
+     $nrows=mysqli_num_rows($result);  //Data from each row from $result is taken through the mysqli_num_rows function and then sent to $nrows
+     
+     //Create a loop that returns each row of data until there is no more data left while there is at least one row of data
+     if ($nrows>0){  
+         echo "<table>";    //Print out table of data
+             echo "<tr>";   //Print out a row for data to be filled in
+                 echo "<th>UserID</th>";    //Print out the queried columns for printed row
+                 echo "<th>Amount</th>";
+                 echo "<th>Position</th>";
+                 echo "<th>Name</th>";
+             echo "</tr>";
+        while ($row=mysqli_fetch_assoc($result)) {  
+             echo "<tr>";   //Print out next row after columns have been specified previously
+                 echo "<td>";
+                 echo $row['userid'];   //echo corresponding data for each column into the new row
+                 echo "</td>";          
+                 echo "<td>";
+                 echo $row['amount'];
+                 echo "</td>";
+                 echo "<td>";
+                 echo $row['position'];
+                 echo "</td>";
+                 echo "<td>";
+                 echo $row['fname'];
+                 echo "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+     }
+     else {
+        echo "There are no salary records<br>";// If there is no data, this message will be displayed
      }
      
      echo "Disconnecting now<br>";

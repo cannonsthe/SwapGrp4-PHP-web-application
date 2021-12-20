26a5275239e92212bca522b09bd9969a75f625ac
Hxsolo2 <2002190C@student.tp.edu.sg>
2021-12-20 10:09:30 +0800

    name change

--- Salary_Add.php
+++ Salary_Add.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Employee Salary</title>
-    <link rel="stylesheet" href="AddSalary.css">
+    <link rel="stylesheet" href="Salary_add.css">
 </head>
 <body>
     <div class="navbar">

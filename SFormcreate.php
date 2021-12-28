b544cbce567d802ace33d01a20de5461c790e4c3
Hxsolo2 <2002190C@student.tp.edu.sg>
2021-12-28 11:39:36 +0800

    test

---
+++ SFormcreate.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Employee Salary</title>
+</head>
+<body>
+
+     <b>Add Record</b><br>
+     <form action="SProcessadd.php" method="post">
+         Amount: <input type="text" name="amount"><br>
+         Position: <input type="text" name="position"><br>
+         Name: <input type="text" name="name"><br>
+         <input type="submit" value="Submit">
+     </form>
+
+
+
+
+
+    
+</body>
+</html>

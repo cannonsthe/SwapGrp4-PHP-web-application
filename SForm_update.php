dbc4e56bce3aed2ebde4985212b27ee4904a6c9b
Hxsolo2 <2002190C@student.tp.edu.sg>
2022-01-04 13:04:13 +0800

    test

---
+++ SForm_update.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EMployee Salary</title>
+    <link rel="stylesheet" href="Salary.css">
+</head>
+<body>
+
+    <div class="navbar">   <!--Navigation bar-->
+       <a href="#" class="logo">FRecords</a>
+       <div class="sections">                                        
+           <a href="Employee.php" >Employee Information</a>
+           <a href="#" class="active">Employee Salary</a>
+           <a href="Expenses.php" > General Expenses</a>
+           <a href="CPF.php">CPF Contributions</a>
+           <a href="feedback.php">Feedback</a>
+           <a href="index.php">Logout</a>
+       </div>
+    </div>
+
+
+    <?php
+     
+     $con = mysqli_connect("localhost","root","","swaprj");  //Open a connection to database server(localhost,user,password,dbname)
+     
+     //Check if the connection works or not
+     if (!$con){
+        die('Failed to connect: ' .mysqli_connect_errno()); //Terminate current script and show error code
+     }
+
+     //Query swaprj database and select data from salary table
+     $query="SELECT userid, amount, position, fname FROM swaprj.salary ". "WHERE userid=" . $_GET['UserID'];
+     $result=mysqli_query($con, $query);
+
+     //Getting the rows of data from the salary table
+     $nrows=mysqli_num_rows($result); 
+
+
+    //Create variables for the data in the salary table
+     if ($nrows>0){
+         $row=mysqli_fetch_assoc($result);
+         $userid=$row['userid'];
+         $amount=$row['amount'];
+         $position=$row['position'];
+         $name=$row['fname'];
+     }
+     else{
+         echo "There are no records<br>";
+     }
+
+    ?>
+    
+    
+    <!--Create form for updating-->
+    <form action="SProcess_update.php" method="post">
+    <input type="hidden" name="id" value="<?php echo $_GET['userid']; ?>"><br>
+    Amount: <input type="text" name="amount" value="<?php echo $amount; ?>"><br>
+    Position: <input type="text" name="position" value="<?php echo $position; ?>"><br>
+    Name: <input type="text" name="name" value="<?php echo $name; ?>"><br>
+    <input type="submit" value="Click to update records">
+    </form>
+
+    
+</body>
+</html>
---
+++ SProcess_update.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Employee Salary</title>
+    <link rel="stylesheet" href="Salary.css">
+</head>
+<body>
+
+    <div class="navbar">   <!--Navigation bar-->
+       <a href="#" class="logo">FRecords</a>
+       <div class="sections">                                        
+           <a href="Employee.php" >Employee Information</a>
+           <a href="#" class="active">Employee Salary</a>
+           <a href="Expenses.php" > General Expenses</a>
+           <a href="CPF.php">CPF Contributions</a>
+           <a href="feedback.php">Feedback</a>
+           <a href="index.php">Logout</a>
+       </div>
+    </div>
+
+    <?php
+
+    $userid=$_POST['userid'];
+    $amount=$_POST['amount'];
+    $position=$_POST['position'];
+    $name=$_POST['fname'];
+
+    $con = mysqli_connect("localhost","root","","swaprj");  //Open a connection to database server(localhost,user,password,dbname)
+     
+     //Check if the connection works or not
+     if (!$con){
+        die('Failed to connect: ' .mysqli_connect_errno()); //Terminate current script and show error code
+     }
+
+    //UPDATE sql query 
+    $query="UPDATE swaprj.salary SET amount='$amount', position='$position', fname='$name' WHERE userid='$userid' ";
+    $result=mysqli_query($con,$query);
+    if (!$result){
+        echo ("UPDATE Failed<br>");
+        echo mysqli_error($con)."<br>";
+    }
+    else{
+       echo "UPDATE OK<br>";
+    }
+
+
+    ?>
+    
+</body>
+</html>

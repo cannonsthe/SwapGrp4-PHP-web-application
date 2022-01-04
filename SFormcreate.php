e9444818a7f7cada2dc4696f3803d070fab73d4c
Hxsolo2 <2002190C@student.tp.edu.sg>
2022-01-04 13:22:44 +0800

    test

--- SFormcreate.php
+++ SFormcreate.php
@@ -23,7 +23,7 @@
 
      <b>Add Records</b><br>
      <form action="SProcesscreate.php" method="post">        <!--Create form for adding record-->
-         UserID: <input type="text" name-="id"><br>
+         UserID: <input type="text" name-="userid"><br>
          Amount: <input type="text" name="amount"><br>
          Position: <input type="text" name="position"><br>
          Name: <input type="text" name="fname"><br>

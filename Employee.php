4c2e8213967e420f25a7481e6ae23c4b9f0886bc
iffah <2004908F@student.tp.edu.sg>
2022-01-24 21:57:47 +0800

    Small employee php changes

--- Employee.php
+++ Employee.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CPF</title>
+    <title>Employee Information</title>
     <link rel="stylesheet" href="Employee.css">
 </head>
 <body>
@@ -21,7 +21,7 @@
        </div>
     </div>
 
-    <strong><p style="font-size:40px;margin-left:36%;">Employee Information</p><strong>
+    <strong><p style="font-size:40px;margin-left:33%;">Employee Information</p><strong>
     
     <form action="Employee_create.php">
         <input type="submit" value="Add User Information">

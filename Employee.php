e27117daeb3da8d3e245dfe229f404f70b11bb0f
iffah <2004908F@student.tp.edu.sg>
2022-01-24 20:07:27 +0800

    Employee php change

--- Employee.php
+++ Employee.php
@@ -22,6 +22,12 @@
     </div>
 
     <strong><p style="font-size:40px;margin-left:36%;">Employee Information</p><strong>
+    
+    <form action="Employee_create.php">
+        <input type="submit" value="Add User Information">
+    </form>
+
+    <br>
 
     <?php
     
@@ -29,16 +35,87 @@
 
     
         
-        $stmt = $conn->prepare("SELECT * FROM swaprj.user_information");
-        $res = $stmt->execute();
-        $stmt->bind_result($userid,$fname,$w_email,$department,$dateregistered,$position,$password,$bankname,$bankaccount,$cpfoa,$cpfsa,$cpfms,$p_email);
-        $stmt->store_result();
-        echo "<table border='1'>";
-        echo "<tr><th>"."User ID"."</th><th>"."Full Name"."</th><th>"."Work Email"."</th><th>"."Department"."</th><th>"."Date Registered"."</th><th>"."Position"."</th><th>"."Password"."</th><th>"."Bank Name"."</th><th>"."Bank Account"."</th><th>"."CPF Ordinary Account"."</th><th>"."CPF Special Account"."</th><th>"."CPF Medishield"."</th><th>"."Personal Email"."</th></tr>";
-        while($stmt->fetch()){
-            echo "<tr><td>".$userid."</td><td>".$fname."</td><td>".$w_email."</td><td>".$department."</td><td>".$dateregistered."</td><td>".$position."</td><td>".$password."</td><td>".$bankname."</td><td>".$bankaccount."</td><td>".$cpfoa."</td><td>".$cpfsa."</td><td>".$cpfms."</td><td>".$p_email."</td></tr>";
+        //Query swaprj database and select data from user information table
+        $query="SELECT * FROM swaprj.user_information"; 
+        $result=mysqli_query($conn,$query); 
+     
+        //Getting the rows of data from the user information table
+        $nrows=mysqli_num_rows($result);  //Data from each row from $result is taken through the mysqli_num_rows function and then sent to $nrows
+     
+        //Create a loop that returns each row of data until there is no more data left while there is at least one row of data
+        if ($nrows>0){  
+         echo "<table border='1'>";    //Print out table of data
+             echo "<tr>";   //Print out a row for data to be filled in
+                 echo "<th>User ID</th>";    //Print out the queried columns for printed row
+                 echo "<th>Full Name</th>";
+                 echo "<th>Work Email</th>";
+                 echo "<th>Department</th>"; 
+                 echo "<th>Date Registered</th>";   
+                 echo "<th>Position</th>";
+                 echo "<th>Password</th>";
+                 echo "<th>Bank Name</th>";
+                 echo "<th>Bank Account</th>";
+                 echo "<th>CPF Ordinary Account</th>";
+                 echo "<th>CPF Special Account</th>";
+                 echo "<th>CPF Medishield Account</th>";
+                 echo "<th>Personal Email</th>";
+                 echo "<th>Update</th>";
+                 echo "<th>Delete</th>";
+             echo "</tr>";
+        while ($row=mysqli_fetch_assoc($result)) {  
+             echo "<tr>";   //Print out next row after columns have been specified previously
+                 echo "<td>";
+                 echo $row['userid'];   //echo corresponding data for each column into the new row
+                 echo "</td>";          
+                 echo "<td>";
+                 echo $row['fname'];
+                 echo "</td>";
+                 echo "<td>";
+                 echo $row['w_email'];
+                 echo "</td>";
+                 echo "<td>";
+                 echo $row['department'];
+                 echo "</td>";
+                 echo "<td>";
+                 echo $row['dateregistered'];
+                 echo "</td>";
+                 echo "<td>";
+                 echo $row['position'];
+                 echo "</td>";
+                 echo "<td>";
+                 echo $row['password'];
+                 echo "</td>";
+                 echo "<td>";
+                 echo $row['bankname'];
+                 echo "</td>";
+                 echo "<td>";
+                 echo $row['bankaccount'];
+                 echo "</td>";
+                 echo "<td>";
+                 echo $row['cpfoa'];
+                 echo "</td>";
+                 echo "<td>";
+                 echo $row['cpfsa'];
+                 echo "</td>";
+                 echo "<td>";
+                 echo $row['cpfms'];
+                 echo "</td>";
+                 echo "<td>";
+                 echo $row['p_email'];
+                 echo "</td>";
+                 echo "<td>";
+                 echo "<a href=Employee_update.php?UserID=" . $row['userid'] . ">Update</a>"; //Create a delete link that replaces old id with new id in another php page,UserID is to take the current id before deletion and store it for deletion
+                 echo "</td>";
+                 echo "<td>";
+                 echo "<a href=Employee_delete.php?UserID=" . $row['userid'] . ">Delete</a>";
+                 echo "</td>";
+            echo "</tr>";
         }
         echo "</table>";
+     }
+     else {
+        echo "There are no salary records<br>";// If there is no data, this message will be displayed
+     }
 
     ?>
 </body>

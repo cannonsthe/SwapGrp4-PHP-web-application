d1a887700ada0c006d28e8d4cd0e3c06d0c2c71c
gabrielle <gabrielletan13@gmail.com>
2022-01-25 23:33:43 +0800

    feedback updates

--- feedback_update.php
+++ feedback_update.php
@@ -23,43 +23,62 @@
        </div>
     </div>
     <br><br>
-    <div class="container">
-    <form action="feedback_create.php" method="get">
 
-    
+    <?php
+     
+        include 'connect.php';
+
+        //Query swaprj database and select data from salary table
+        $query="SELECT * FROM swaprj.feedback WHERE userid=" . $_GET['userid']; 
+        $result=mysqli_query($conn, $query);
+
+        //Getting the rows of data from the salary table
+        $nrows=mysqli_num_rows($result); 
+        if ($nrows>0) {                       //Create variables for the data in the salary table
+            $row=mysqli_fetch_assoc($result);
+            $userid=$row['userid'];
+            $feedbackid=$row['feedbackid'];
+            $fname=$row['fname'];
+            $subject=$row['subject'];
+            $email=$row['email'];
+            $feedback=$row['feedback'];
+            $department=$row['department'];
+            
+        }
+        else{
+            echo "There are no records.<br>";
+        }
+    ?>
+    <div class="container">
+    <form action="feedback_update2.php" method="get">
+
+
+<label for="userid">User ID</label>
+<input type="text" name="userid" value="<?php echo $_GET['userid']; ?>">
+
+<label for="feedbackid">Feedback ID</label>
+<input type="text" name="feedbackid" value="<?php echo $feedbackid; ?>">
+
 <label for="fname">Name</label>
-<input type="text" name="fname">
+<input type="text" name="fname" value="<?php echo $fname; ?>">
 <!---id="fname" placeholder="Your name"--->
 <label for="subject">Subject</label>
-<input type="text" name="subject">
+<input type="text" name="subject" value="<?php echo $subject; ?>">
 <!---id="subject" placeholder="Subject"--->
 <label for="email">Email</label>
-<input type="text" name="email">
+<input type="text" name="email" value="<?php echo $email; ?>">
 <!---id="email" placeholder="Your email"--->
 <label for="feedback">Feedback</label>
-<textarea style="height:200px" name="feedback"></textarea>
+<textarea style="height:200px" name="feedback" value="<?php echo $feedback; ?>"></textarea>
 <!---id="feedback" placeholder="Feedback"--->
 <label for="department">Department</label>
-<input type="text" name="department">
+<input type="text" name="department" value="<?php echo $department; ?>">
 
 
 
 
 <input type="submit" value="Update">
 
+
 </form>
-<?php
-include 'connect.php';
-
-$stmt = $conn->prepare("UPDATE feedback set fname=?, email=?, subject=?, feedback=?, department=? WHERE userid=? ");
-$stmt->bind_param("sssss", $email, $subject, $sfeedback, $department, $fname);
-$res = $stmt->execute();
-    echo "<form action='feedback_create.php' method='get'>";
-    echo "<label for='fname'>Name</label>
-    <input type='text' name='name'>".$fname
-                        
-                        
-            		
-
-?>
 </body>

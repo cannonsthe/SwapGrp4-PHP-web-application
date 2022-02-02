8d77b23e16900ad4d96b4ecf76e1ef4487084764
Hxsolo2 <2002190C@student.tp.edu.sg>
2022-02-02 13:19:23 +0800

    test

--- SForm_create.php
+++ SForm_create.php
@@ -23,9 +23,9 @@
 
      <b>Add Records</b><br>
      <form action="SProcess_create.php" method="post">        <!--Create form for adding record-->
-         UserID: <input type="number" name="userid"  pattern="[$]+[0-9]+[SGD]" title="Must contain a $ sign with numbers from 0-9, and the word SGD" required><br>
-         Amount: <input type="text" name="amount" required><br>
-         Position: <input type="text" maxlength name="position" required><br>
+         UserID: <input type="number" name="userid" required><br>
+         Amount: <input type="text" name="amount" pattern="[$]+[0-9]+[SGD]" title="Must contain a $ sign with numbers from 0-9, and the word SGD" required><br>
+         Position: <input type="text" name="position" required><br>
          Name: <input type="text" name="fname" required><br>
          <input type="submit" value="Submit">
      </form>
